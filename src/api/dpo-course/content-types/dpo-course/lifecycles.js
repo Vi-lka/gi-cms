0588e3c2ca879ba18538efa8a8999ae5537d97e7
Vi-lka <vitalya.permyakov155@gmail.com>
2024-04-26 22:36:45 +0700

    fix: create slug only in default locale

--- src/api/dpo-course/content-types/dpo-course/lifecycles.js
+++ src/api/dpo-course/content-types/dpo-course/lifecycles.js
@@ -2,48 +2,24 @@ let slugify = require("slugify");
 const { ApplicationError } = require("@strapi/utils").errors;
 
 module.exports = {
-    async beforeCreate(event) {
-        const { data } = event.params
-        const id = event.params?.where?.id ?? null;
-        console.log("beforeCreate", event.params.data)
+  async beforeCreate(event) {
+      const { data } = event.params
+      console.log("beforeCreate", event.params)
 
-        await generateSlug(id, data);
-    },
-
-    async afterCreate(event) {
-        const { data } = event.params
-        const id = event.params?.where?.id ?? null;
-        console.log("afterCreate", event.params.data)
-
-        await generateSlug(id, data);
-    },
-
-    async beforeUpdate(event) {
-        const { where } = event.params
-        const id = where.id;
-
-        const data = await strapi.entityService.findOne("api::dpo-course.dpo-course", id)
-
-        await generateSlug(id, data);
-    }
-  };
+      await generateSlug(data);
+  },
+};
   
-  const generateSlug = async (id, data) => {
-    const DEFAULT_LOCALE = "ru";
-    const locale = !id ? "ru" : await getLocale(id);
-  
-    //Generate slug for en locale only
-    if (data.title && locale == DEFAULT_LOCALE) {
-      data.slug = slugify(data.title, { lower: true });
-    }
-  
-    if (!data.slug) {
-      throw new ApplicationError("Slug is required!");
-    }
-  };
-  
-  const getLocale = async (id) => {
-    const res = await strapi.service("api::dpo-course.dpo-course").findOne(id);
-  
-    return res.locale;
-  };
+const generateSlug = async (data) => {
+  const DEFAULT_LOCALE = "ru";
+  console.log("locale: ", data.locale)
+
+  //Generate slug for ru locale only
+  if (data.title && data.locale === DEFAULT_LOCALE) {
+    data.slug = slugify(data.title, { lower: true });
+  }
+
+  if (!data.slug) {
+    throw new ApplicationError("Slug is required!");
+  }
+};
